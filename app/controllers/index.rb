9ea5282069855eab536b028c6493f714a81e1996
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-02 21:28:57 -0500

    .

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,14 +5,22 @@ end
 post '/game2' do
   @player1 = Player.create(:name => params[:player_one])
   @player2 = Player.create(:name => params[:player_two])
-  @game = Game.create(:player_one => @player_one, :player_two => @player_two)
+  
+  @game = Game.create(:player_one => @player1.name, :player_two => @player2.name)
+  @game.players << @player1
+  @game.players << @player2
+ 
   session[:game_id] = @game.id
   erb :game2
 end
 
 post '/save_time' do 
+  p params.inspect
   @game = Game.find(session[:game_id])
+  @player1 = @game.players[0]
+  @player2 = @game.players[1]
   @game.duration = params[:duration]
+  p @game
   erb :game2
 end
 
--- db/migrate/20130902180107_create_players.rb
+++ db/migrate/20130902180107_create_players.rb
@@ -1,6 +1,7 @@
 class CreatePlayers < ActiveRecord::Migration
   def change
     create_table :players do |t|
+      t.belongs_to :game
       t.string :name 
       t.timestamps
     end
